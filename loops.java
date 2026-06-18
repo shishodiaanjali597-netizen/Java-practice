b405814ddcc68226ca95edbe4f3b435b4c5bb359
Anjali <shishodiaanjali597@gmail.com>
2026-06-18 10:54:07 +0530

    Updated java codes

--- loops.java
+++ loops.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class loops{
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
+        /*Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
 
         int sum=0;
@@ -10,7 +10,137 @@ public class loops{
             sum=sum+i;
 
         }
-        System.out.println(sum);
+        System.out.println(sum);*/
+
+        // to print the multiplication table of a number
+        /*Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+(n*i));
+        }*/
+
+            /*  while loop code*/
+/* To print natural no. from 100-200 */
+ /*public class Loops{
+    public static void main(String [] args){
+        int i=100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}*/
+/*public class Loops{
+    public static void main(String[] args){
+        int sum=0;
+        int n=3;
+        int i=0;
+        while(i<=n){
+            sum=sum+(2*i);
+            i++;
+        }
+        System.out.println("The sum of first n even number is "+sum);
+    }
+}*/
+
+
+// Do while loop code//
+/*public class Loops{
+
+    public static void main(String [] args){
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=50);
+
+    }
+}*/
+
+//For loop//
+/*public class Loops{
+    public static void main(String [] args){
+        int n=10;
+        for(int i=0;i<n;i++){
+            System.out.println(2*i+1);// To print odd number 
+        }
+    }
+
+}*/
+/*public class Loops{
+    public static void main(String [] args){
+        
+        for(int i=10;i>0;i--){
+            System.out.println(i);
+        }
+    }
+
+}*/
+/*public class Loops{
+    public static void main(String[] args){
+        int sum=0;
+        int n=3;
+        for(int i=0;i<n;i++){
+            sum=sum+(2*i);
+            
+        }
+        System.out.println("The sum of first n even number is "+sum);
+    }
+}*/
+
+/*public class Loops{
+    public static void main(String[]args){
+        int n=2;
+        int x=0;
+        for(int i=1;i<=10;i++){
+            x=n*i;
+            System.out.println(n+"*"+i+"="+x);
+
+        }
+        
+    }
+}*/
+/*public class Loops{
+    public static void main(String[]args){
+        int n=8;
+        int x=0;
+        for(int i=10;i>=1;i--){
+            x=n*i;
+            System.out.println(n+"*"+i+"="+x);
+
+        }
+        
+    }
+}*/
+/*public class Loops{
+    public static void main(String[]args){
+        int n=2;
+        int x=0;
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            x=n*i;
+            System.out.println(n+"*"+i+"="+x);
+            sum=sum+x;
+            
+
+        }
+        System.out.println("the sum of numbers accuring in the multiplication of 2="+sum);
+        
+    }
+}*/
+/*public class Loops{
+    public static void main(String[]args){
+        int n=5;
+        int factorial=1;
+        int i=1;
+        while(i<=n){
+            factorial=factorial*i;
+            i++;
+        }
+        System.out.println("factorial="+factorial);
+    }
+}*/
 
     }
 }
