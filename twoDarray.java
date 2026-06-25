d3a27d703f059878a3e6d673069f2fca6eda5f9c
Anjali <shishodiaanjali597@gmail.com>
2026-06-25 11:10:07 +0530

    Updated Two D Array codes

---
+++ twoDarray.java
@@ -0,0 +1,84 @@
+/public class twoDarray {
+    //public static void main(String[]args){
+        //int arr[][]={{1,2,3}
+                     //,{4,5,6}
+                     //,{7,8,9}};
+        //TO PRINT 2-D ARRAY
+        /*int row=arr.length;
+        int col=arr[0].length;
+        for(int i=0;i<=row-1;i++){
+            for(int j=0;j<=col-1;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }*/
+        //TO PRINT SUM OF 2-D ARRAY
+        /*int sum=0;
+        int row=arr.length;
+        int col=arr[0].length;
+        for(int i=0;i<=row-1;i++){
+            for(int j=0;j<=col-1;j++){
+                sum=sum+arr[i][j];
+            }
+            
+        }
+        System.out.println("The sum of 2-D array is = "+sum);*/
+        //TO PRINT MULTIPLICATION OF 2-D ARRAY
+        /*int ans=1;
+        int row=arr.length;
+        int col=arr[0].length;
+        for(int i=0;i<=row-1;i++){
+            for(int j=0;j<=col-1;j++){
+                ans=ans*arr[i][j];
+            }
+            
+        }
+        System.out.println("The multiplication of 2-D array is = "+ans);*/
+        //TO PRINT DIAGONAL ELEMENT OF 2-D ARRAY
+        /*int row=arr.length;
+        int col=arr[0].length;
+        for(int i=0;i<=row-1;i++){
+            for(int j=0;j<=col-1;j++){
+                if(i==j){
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+            
+        }*/
+       //TO PRINT ROW-WISE SUM OF 2-D ARRAY
+       /*int row=arr.length;
+       for(int i=0;i<=row-1;i++){
+           int sum=0;
+           for(int j=0;j<=arr[0].length-1;j++){
+               sum=sum+arr[i][j];
+           }
+           System.out.println("Sum of row "+i+" is = "+sum);
+       }
+       System.out.println();*/
+         //TO PRINT COLUMN-WISE SUM OF 2-D ARRAY
+         /*int col=arr[0].length;
+            for(int j=0;j<=col-1;j++){
+                int sum=0;
+                for(int i=0;i<=arr.length-1;i++){
+                    sum=sum+arr[i][j];
+                }
+                System.out.println("Sum of column "+j+" is = "+sum);
+            }*/
+             //TO PRINT TRANSPOSE OF 2-D ARRAY
+             /*int row=arr.length;
+             int col=arr[0].length;
+             int transpose[][]=new int[col][row];
+             for(int i=0;i<=row-1;i++){
+                 for(int j=0;j<=col-1;j++){
+                     transpose[j][i]=arr[i][j];
+                 }
+             }
+             for(int i=0;i<=col-1;i++){
+                 for(int j=0;j<=row-1;j++){
+                     System.out.print(transpose[i][j]+" ");
+                 }
+                 System.out.println();
+             }*/
+         //}
+    
+//}
